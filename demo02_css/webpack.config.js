97247e4f20b3478018260044268aff9310e8a595
蔡敏 <c17612129501@163.com>
2020-04-03 10:59:36 +0800

    文档

--- demo02_css/webpack.config.js
+++ demo02_css/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -28,7 +29,8 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "style/[name].css"
-        })
+        }),
+        new CleanWebpackPlugin()
     ],
     devServer: {
         contentBase: "./dist", //本地服务器所加载的页面所在的目录
